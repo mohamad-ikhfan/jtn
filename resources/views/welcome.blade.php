6572674ad1e075dfce629cb06f94edde1c224dc7
mohamad-ikhfan <mohamad.ikhfan@gmail.com>
2023-05-09 15:09:42 +0700

    upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -866,7 +866,7 @@
                         </svg>
                     </a>
 
-                    <a href="/register"
+                    {{-- <a href="/register"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 hover:outline-4 hover:outline-red-500 border-4 border-transparent hover:border-red-500">
                         <div>
                             <div
@@ -888,7 +888,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                         </svg>
-                    </a>
+                    </a> --}}
 
                     <a href="{{ url('http://49.0.2.107/') }}" target="blank"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 hover:outline-4 hover:outline-red-500 border-4 border-transparent hover:border-red-500">
@@ -953,8 +953,8 @@
                             </p>
                         </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                         </svg>
